a0474411bdf4337a024c04898bcd8be8edd06f3a
EEV <erinvaca@erin-vacas-imac.gateway.2wire.net>
2014-06-18 23:10:32 -0700

    Assignment 2 submission
    This is the code that finally worked for me.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script is intended to cache the inverse of a matrix when it is calculated 
+## the first time and to return the cached value rather than recalculate when
+## the function cacheSolve is repeatedly called.  
 
-## Write a short comment describing this function
+## This code was tested on the following matrix:
+## m1 <- matrix(data = c(1,3,3,1,4,3,1,3,4), nrow =3, byrow=TRUE)
+
+## makeCacheMatrix stores the inverse value of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mycache <- NULL
+    get <- function() x
+    setinv <- function(newinv) mycache <<- newinv
+    getinv <- function() mycache
+    list(get = get,
+         setinv = setinv,
+         getinv = getinv)
+    
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks to see if there is is any cached value for the matrix inverse.
+## If not, cacheSolve calculates the inverse and passes that value to mycache for 
+## subsequent retrieval.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    mycache <- x$getinv()
+    if(!is.null(mycache)) {
+        message("getting cached data")
+        return(mycache)
+    }
+    data <- x$get()
+    mycache <- solve(data, ...)
+    x$setinv(mycache)
+    mycache
 }
